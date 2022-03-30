ec9049c7f6aa8db470156032d30b6339c7ef4308
monoranjan.das <monoranjan.das@covianalytics.com>
2022-03-31 00:04:45 +0600

    Event handler example

---
+++ src/components/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+        <div class="card shadow-lg">
+            <div class="card-header">
+                <h2 class="text-center">Counter</h2>
+            </div>
+            <div class="card-body">
+                <p class="h3 text-center display-3 my-5">{{this.count}}</p>
+                <div class="row my-3 mx-3">
+                    <div class="col-md-12 display-block text-center">
+                        <button class="btn btn-success col-md-3 mx-3" @click="this.incr">Incremeant</button>
+                        <button class="btn btn-danger  col-md-3 mx-3" @click="this.decr">Decremeant</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    data(){
+        return {
+            count: 0
+        };
+    },
+    methods: {
+        incr() {
+            this.count = this.count + 1;
+        },
+        decr() {
+            this.count = this.count - 1;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Greeting.vue
@@ -0,0 +1,39 @@
+<template>
+     
+     <div class="card shadow-lg">
+         <div class="card-header">
+            <h2 class="text-center">Greeting</h2>
+         </div>
+          <div class="card-body">
+              <p class="h3 text-center display-3 my-5">Hi... <span>{{this.message}}</span></p>
+              <div class="row my-3 mx-3">
+                  <div class="col-md-12 display-block text-center">
+                      <button class="btn btn-success col-md-2 mx-1" @click="this.greet('Good Morning')">Morning</button>
+                      <button class="btn btn-warning  col-md-2 mx-1" @click="this.greet('Good Afternoon')">Afternoon</button>
+                      <button class="btn btn-danger  col-md-2 mx-1" @click="this.greet('Good Evening')">Evening</button>
+                      <button class="btn btn-dark  col-md-2 mx-1" @click="this.greet('Good Night')">Night</button>
+                  </div>
+               </div>
+           </div>
+       </div>
+</template>
+
+<script>
+export default {
+    name: 'Greeting',
+    data: function(){
+        return {
+            message: ''
+        }
+    },
+    methods:{
+        greet(value){
+            this.message = value;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
