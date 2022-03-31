cfb5f50ce9ed5470d0c10498e8f756477264584d
monoranjan.das <monoranjan.das@covianalytics.com>
2022-03-31 11:52:34 +0600

    Implement loading spiner

---
+++ src/components/Spiner.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="../assets/loading.gif" alt="loading" class="d-block m-auto"/>
+</template>
+
+<script>
+export default {
+    name: 'Spiner'
+}
+</script>
+
+<style scoped>
+
+</style>
